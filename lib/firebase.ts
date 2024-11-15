a62f0b9761043944854129741aad9daed33ca3a4
ali <alihasank86@gmail.com>
2024-11-15 16:20:28 +0530

    update firebase config and change database provider

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -7,13 +7,13 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCTyR2waoDNE_6EgOCD-cDQRlcs5HyWf3",
-  authDomain: "file-612e3.firebaseapp.com",
-  projectId: "file-612e3",
-  storageBucket: "file-612e3.appspot.com",
-  messagingSenderId: "61791378211",
-  appId: "1:617913782112:web:98c8baa436d44754049805",
-  measurementId: "G-V681QYLQ5L",
+  apiKey: "AIzaSyBwyu1_e9rPXbQzSbkorqb6cc5PiSk9J8s",
+  authDomain: "school-management-system-4627d.firebaseapp.com",
+  projectId: "school-management-system-4627d",
+  storageBucket: "school-management-system-4627d.firebasestorage.app",
+  messagingSenderId: "515278284203",
+  appId: "1:515278284203:web:c0cc14b869e56f3e195eee",
+  measurementId: "G-41H4EGRQYP"
 };
 
 // Initialize Firebase
